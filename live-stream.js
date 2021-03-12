a25616eca08836c032ba7713328594058e040a6b
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2021-03-12 08:44:38 +0700

    Create live-stream.js

---
+++ live-stream.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : 'b9e955afc2c79c65f2e1570132e3d118',
+	'format' : 'iframe',
+	'height' : 50,
+	'width' : 320,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://installationtray.com/b9e955afc2c79c65f2e1570132e3d118/invoke.js"></scr' + 'ipt>');
