4566950fc0931c94bec71183fcabbe5f2b4ef080
azr-555-secret <62775921+azr-555-secret@users.noreply.github.com>
2021-03-12 08:45:17 +0700

    Create baim.js

---
+++ baim.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : '37a9c0c1a0abd3c72faaf9002ad32461',
+	'format' : 'iframe',
+	'height' : 50,
+	'width' : 320,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://installationtray.com/37a9c0c1a0abd3c72faaf9002ad32461/invoke.js"></scr' + 'ipt>');
